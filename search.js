a54ddbe80a0563e6200a016f7e86c746e5996dd4
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2022-08-12 04:07:45 -0700

    html vchange

--- search.js
+++ search.js
@@ -3,17 +3,23 @@
 //
 
 function createLielement(task) {
+  const color = {};
   const node = document.createElement("li");
-  node.innerHTML = `<div>
+  node.innerHTML = `<div class="parent">
                       <img src="asse/edit.png" class="edit in" />
+                      <div>
                       <h3 class="in">${task.text}</h3>
-                      <p class="pl">${task.category}</h3>
-                      <p class="pl">${task.time}</p>
-                      <p class="pl">${task.date}</p>
+                      <p class="in bl">${task.desc}</p></div>
+                      <div class="cata"></div>
+                      
                    </div>`;
   return node;
 }
-
+{
+  /* <p class="pl">${task.category}</h3>
+                      <p class="pl">${task.time}</p>
+                      <p class="pl">${task.date}</p> */
+}
 window.onload = () => {
   tasks = JSON.parse(localStorage.getItem("todos"));
   const task_list = document.querySelector("#task_list");
