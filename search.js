5b41957ca9c361409e5447dacfc3c75dec089938
mohammadestare <mohammadestare@gmail.com>
2022-08-12 14:38:21 +0430

    adding js to load list of taks

--- search.js
+++ search.js
@@ -1,7 +1,23 @@
-const search = document.getElementById("search");
-const search_icon = document.getElementById("searchicon");
-search_icon.addEventListener("click", function searchh() {
-  {
-    search.value = localStorage.getItem("todos");
-  }
-});
+//
+//
+//
+
+function createLielement(task) {
+  const node = document.createElement("li");
+  node.innerHTML = `<div>
+                      <img src="asse/edit.png" class="edit in" />
+                      <h3 class="in">${task.text}</h3>
+                      <p class="pl">${task.category}</h3>
+                      <p class="pl">${task.time}</p>
+                      <p class="pl">${task.date}</p>
+                   </div>`;
+  return node;
+}
+
+window.onload = () => {
+  tasks = JSON.parse(localStorage.getItem("todos"));
+  const task_list = document.querySelector("#task_list");
+  tasks.forEach(function (task) {
+    task_list.appendChild(createLielement(task));
+  });
+};
