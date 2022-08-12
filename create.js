f7bd66f9c906c07fce46a3fbf89c4d8b14062e0c
mohammadestare <mohammadestare@gmail.com>
2022-08-12 12:27:09 +0430

    adding js functions

---
+++ create.js
@@ -0,0 +1,13 @@
+// Saving to local storage:
+function savelocal(todo) {
+  //Check: if item/s are there;
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
