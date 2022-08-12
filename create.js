5261e9b5884224b267f2303bddc2314f5e0354b5
mohammadestare <mohammadestare@gmail.com>
2022-08-12 13:01:40 +0430

    adding local storage and onclick function

--- create.js
+++ create.js
@@ -11,3 +11,16 @@ function savelocal(todo) {
   todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function submit_infos() {
+  const inputs = document.querySelectorAll("input");
+  information = {};
+  inputs.forEach(function (input) {
+    if (input.type === "radio" && input.checked === false) {
+      return;
+    }
+    information[input.name] = input.value;
+  });
+  console.log(information);
+  savelocal(information);
+}
